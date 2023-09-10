f77471f916069568f3473480b4504f543e6538b2
Alexandre <alexprazeres11@hotmail.com>
2023-09-09 23:06:33 -0300

    initial fork

---
+++ lib/src/page.dart
@@ -0,0 +1,50 @@
+import 'dart:io';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_advanced_networkimage_2/zoomable.dart';
+
+class PDFPage extends StatefulWidget {
+  final String imgPath;
+  final int num;
+  PDFPage(this.imgPath, this.num);
+
+  @override
+  _PDFPageState createState() => _PDFPageState();
+}
+
+class _PDFPageState extends State<PDFPage> {
+  late ImageProvider provider;
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    _repaint();
+  }
+
+  @override
+  void didUpdateWidget(PDFPage oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (oldWidget.imgPath != widget.imgPath) {
+      _repaint();
+    }
+  }
+
+  _repaint() {
+    provider = FileImage(File(widget.imgPath));
+    final resolver = provider.resolve(createLocalImageConfiguration(context));
+    resolver.addListener(ImageStreamListener((imgInfo, alreadyPainted) {
+      if (!alreadyPainted) setState(() {});
+    }));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        decoration: null,
+        child: ZoomableWidget(
+          zoomSteps: 3,
+          minScale: 1.0,
+          maxScale: 3.0,
+          child: Image(image: provider),
+        ));
+  }
+}
